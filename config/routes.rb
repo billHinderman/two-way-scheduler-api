d57a6ef707ccde902701d40b6be877f1c3482190
Bill Hinderman <bill@billhinderman.com>
2019-10-02 15:08:36 -0500

    Reformatting routes, scoping likely models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # API SUBDOMAIN
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      # USERS
+      scope '/users' do
+          # USER/EMPLOYER
+          # USER/TALENT
+      end
+
+      # APPOINTMENTS
+      scope '/appointments' do
+      end
+
+      # CALENDARS
+      scope '/calendars' do
+      end
+
+      # INTERVIEWS
+      scope '/interviews' do
+      end
+
+    end
+  end
 end
